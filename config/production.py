eee2989eae4cd1a75285c3e43b2f73331072b155
cs9601325 <cs9601325@gmail.com>
2022-11-29 10:11:00 +0900

    log 생성

--- config/production.py
+++ config/production.py
@@ -1,6 +1,30 @@
+from logging.config import dictConfig
 from config.default import *
+
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 SECRET_KEY = b'V\xfe~\xb9j\x0cz\xc57\xb7\x82X\x08w\xf1\x1f'
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
